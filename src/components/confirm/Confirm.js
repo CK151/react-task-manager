8a91aa92051c07b179e689f48d9c5b0e98165a8e
CK151 <andersonoki15@gmail.com>
2023-03-17 12:08:10 +0300

    init

---
+++ src/components/confirm/Confirm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Confirm.css"
+
+const Confirm = ({modalTitle, modalMsg, modalAction, modalActionText, onCloseModal}) => {
+  return (
+    <div className='confirm'>
+      <div className='confirm-modal'>
+        <div className='header'>
+          <span className='title'>{modalTitle}</span>
+          <button className='close' onClick={onCloseModal}>&times;</button>
+        </div>
+        <div className='content'>
+          <p>{modalMsg}</p>
+        </div>
+        <div className='buttons'>
+          <button className='btn btn-ok' onClick={modalAction}>{modalActionText}</button>
+          <button className='btn btn-cancle' onClick={onCloseModal}>cancel</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Confirm
